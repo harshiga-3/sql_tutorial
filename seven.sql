ebea8aff33f73a2a17ad3ec44b3c12a74903ec71
harsh <harshigadevi@gmail.com>
2026-01-30 22:59:10 +0530

    today quesry

--- seven.sql
+++ seven.sql
@@ -20,3 +20,22 @@ SELECT dept, COUNT(dept) AS t_count
 FROM Employee
 GROUP BY dept
 HAVING COUNT(dept) > 5;
+
+1.Display all distinct salaries from the Employee table
+SELECT DISTINCT salary
+FROM Employee;
+
+2. Display department-wise total salary
+SELECT dept, SUM(salary) AS total_salary
+FROM Employee
+GROUP BY dept;
+
+3. Display the maximum salary in each department
+SELECT dept, MAX(salary) AS max_salary
+FROM Employee
+GROUP BY dept;
+
+4. Display all employees sorted by salary in descending order
+SELECT *
+FROM Employee
+ORDER BY salary DESC;
